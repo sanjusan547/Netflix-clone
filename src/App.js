45b8f1ee3f798dc1adf05094c2864b94db2ceaca
sanju <sanjusan547@gmail.com>
2025-07-30 19:03:39 +0530

    Added posters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './components/navbar/navbar'
-
+import Banner from './components/banner/banner'
+import './App.css'
+import RowPost from './components/rowposts/RowPost'
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Banner/>
+      <RowPost/>
     </div>
   )
 }
---
+++ src/components/banner/banner.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './banner.css'
+function Banner() {
+  return (
+    <div className='banner'>
+      <div className='content'>
+        <h1 className='title'>Movie Name</h1>
+      
+      <div className='buttons'>
+        <button className='button1'>Play</button>
+        <button className='button1'>Mylist</button>
+      </div>
+      <h1 className='description'>Money Heist" (also known as "La Casa de Papel") is a Spanish heist crime drama series about a group of robbers who, led by a mysterious figure known as "The Professor," attempt to pull off two ambitious heists: one at the Royal Mint of Spain and another at the Bank of Spain. The series is known for its intricate plot, complex characters, and intense cat-and-mouse games with law enforcement. </h1>
+      </div>
+      <div className='fade'>
+
+      </div>
+    </div>
+    
+
+  )
+}
+
+export default Banner
